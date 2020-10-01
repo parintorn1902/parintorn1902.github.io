35b2aad9a336d0f8e84f097fa7e3a16acae61e4e
parintorn <parintorn1902@gmail.com>
2020-10-01 20:53:27 +0700

    test github hosting with react js

---
+++ precache-manifest.d2899c59f924443eb3dbc4c0201c71f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56eeacaf693efb6047a7881ebe841b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8bff5e501e25a37887",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "a343745739f263da50cb",
+    "url": "/static/js/2.911d97e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.911d97e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8bff5e501e25a37887",
+    "url": "/static/js/main.7724adfd.chunk.js"
+  },
+  {
+    "revision": "a2a4e2290806c91b7577",
+    "url": "/static/js/runtime-main.43a251e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
