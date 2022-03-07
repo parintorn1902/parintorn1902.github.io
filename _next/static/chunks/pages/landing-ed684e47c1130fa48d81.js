f0b9927e2ba6b315834762c8ce53755842b8bd00
parintorn <parintorn1902@gmail.com>
2022-03-07 12:04:30 +0700

    test next static web

---
+++ _next/static/chunks/pages/landing-ed684e47c1130fa48d81.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469],{487:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/landing",function(){return _(9402)}])},1464:function(n){"use strict";n.exports=next}},function(n){n.O(0,[445,937,262,774,888,179],(function(){return u=487,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
